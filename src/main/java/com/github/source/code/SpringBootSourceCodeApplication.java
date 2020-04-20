7a38853b11ec404f6493fbd7935d3784db1555e8
shishaodong <shishaodong@cmhi.chinamobile.com>
2020-04-20 10:47:40 +0800

    Initial commit

---
+++ src/main/java/com/github/source/code/SpringBootSourceCodeApplication.java
@@ -0,0 +1,13 @@
+package com.github.source.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSourceCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSourceCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/source/code/SpringBootSourceCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.source.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSourceCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
